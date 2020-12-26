1e8a1104921361d9198bf5ed4306beea96143133
Cyril Leclerc <cyril@dashlane.com>
2020-12-26 12:36:23 +0100

    tf13

--- versions.tf
+++ versions.tf
@@ -1,9 +1,21 @@
 terraform {
-  required_version = ">= 0.12"
+  required_version = ">= 0.13"
   required_providers {
-    aws      = ">= 2.53.0"
-    tls      = ">= 2.1.1"
-    ignition = ">= 1.2.1"
-    local    = ">= 1.4.0"
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 2.53.0"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = ">= 2.1.1"
+    }
+    ignition = {
+      source  = "terraform-providers/ignition"
+      version = ">= 1.2.1"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = ">= 1.4.0"
+    }
   }
 }
